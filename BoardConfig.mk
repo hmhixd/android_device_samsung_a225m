8f93d600a5b6b701d80417284024605cf86d35e8
hmhixd <101072056+hmhixd@users.noreply.github.com>
2022-03-06 20:46:52 -0300

    This will take a while

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/guamp
+
+# Target
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
