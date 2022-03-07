740a52b54e4bbad12e1c241589827dac8ff30c58
hmhixd <101072056+hmhixd@users.noreply.github.com>
2022-03-06 21:07:30 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a22x.mk
+    $(LOCAL_DIR)/lineage_a225m.mk
     
     COMMON_LUNCH_CHOICES := \
-    lineage_a22x-eng \
-    lineage_a22x-user \
-    lineage_a22x-userdebug
+    lineage_a225m-eng \
+    lineage_a225m-user \
+    lineage_a225m-userdebug
