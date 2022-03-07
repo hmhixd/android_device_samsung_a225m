6ace079a6b6b6a4c045c52b0944da91791642215
hmhixd <101072056+hmhixd@users.noreply.github.com>
2022-03-07 13:39:01 -0300

    o-o

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter guamp, $(TARGET_DEVICE)),)
+ifneq ($(filter a225m, $(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
