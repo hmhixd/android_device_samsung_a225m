1fd8e1c29c5b22e05105d58a4e5d9bbcb81c8913
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-03-13 17:26:51 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a22x
+DEVICE_PATH := device/samsung/a225m
 
 # Target
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := generic
+
+#Paritions
