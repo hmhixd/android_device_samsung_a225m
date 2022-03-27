68567736d381b10aab8255b18b90485ca95214af
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-03-27 15:24:22 -0300

    .

--- lineage_a225m.mk
+++ lineage_a225m.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/a22x/device.mk)
 
 ### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
