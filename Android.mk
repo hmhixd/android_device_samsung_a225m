5cf750b7512489f9cf3b3f78260d90c685853552
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-03-27 21:14:25 -0300

    Delete Android.mk

--- Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(filter a225m, $(TARGET_DEVICE)),)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
-endif
